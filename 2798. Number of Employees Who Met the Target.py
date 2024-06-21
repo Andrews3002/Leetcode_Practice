2fe32bc80074ef544d7bc641791ed3b584123c1a
Andrews3002 <131720759+Andrews3002@users.noreply.github.com>
2024-06-21 09:39:39 -0400

    Create 2798. Number of Employees Who Met the Target.py

---
+++ 2798. Number of Employees Who Met the Target.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        """
+        :type hours: List[int]
+        :type target: int
+        :rtype: int
+        """
+        ans = 0
+
+        for i in hours:
+            if i >= target:
+                ans = ans + 1
+        
+        return ans
+        
