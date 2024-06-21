897947d453c54c8f55dfd403ea59838b64ef481e
Andrews3002 <131720759+Andrews3002@users.noreply.github.com>
2024-06-21 09:23:07 -0400

    Create 20. Valid Parentheses.cpp

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        
+        for (int i = 0; i < s.length(); i++){
+            if (s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else{
+                if (st.empty() == true){
+                    return false;
+                }
+                else if ((s[i] == ')' && st.top() == '(') || (s[i] == '}' && st.top() == '{') || (s[i] == ']' && st.top() == '[')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        
+        if (st.empty() == true){
+            return true;
+        }
+        else {
+            return false;  
+        }
+    } 
+};
+
+
+
